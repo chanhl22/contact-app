18dabc40e8da83da9e8fe790de06388591371bb4
chanhl22 <lch9502@naver.com>
2024-07-16 23:30:16 +0900

    jsx

---
+++ src/Jsx2.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Jsx2() {
+  return (
+    <Fragment>
+      <div>Hello</div>
+      <div>Hello</div>
+    </Fragment>
+  )
+}
+
+export default Jsx2;
