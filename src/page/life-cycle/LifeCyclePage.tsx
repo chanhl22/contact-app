a730443773fc937002e3d9b7ac536461bd5839fe
chanhl22 <lch9502@naver.com>
2024-07-26 17:26:18 +0900

    life cycle

--- src/page/life-cycle/LifeCyclePage.tsx
+++ src/page/life-cycle/LifeCyclePage.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const LifeCyclePage: React.FC = () => {
+    console.log("리액트 실행순서 - 1. 컴포넌트 최상단")
+
+    useEffect(() => {
+        console.log("리액트 실행순서 - 2. useEffect 실행")
+    });
+
+    const value = '리액트 실행순서 - 3. 리턴';
+    const debugValue = (() => {
+        console.log(value);
+        return value;
+      })();
+
     return (
         <div>
-            라이프 사이클 관련
+            라이프 사이클 관련 페이지
+            <br/>
+            {debugValue}
         </div>
     );
 };
