46810ab762905fb30cb4e4f5b55a5e744bd3c64c
chanhl22 <lch9502@naver.com>
2024-11-20 16:24:12 +0900

    feat: props는 부모 컴포넌트에서 전달된 값이고, 한 번 전달된 props는 자식 컴포넌트 내부에서 직접 수정이 되긴함

--- src/page/useeffect-props/UseEffectProps.tsx
+++ src/page/useeffect-props/UseEffectProps.tsx
@@ -9,23 +9,33 @@ interface UseEffectPropsProps {
 const UseEffectProps: React.FC<UseEffectPropsProps> = ({ value, question }) => {
     const [currentValue, setCurrentValue] = useState<number>(() => 20);
 
-    const otherQuestion = {questionId: 300, answerValue: "테스트테스트"};
+    const otherQuestion = {questionId: 300, answerValue: '테스트테스트'};
 
     useEffect(() => {
         value = currentValue;
         console.log(value);
-    }, [value, currentValue]);
+    }, []);
+
+    console.log('자식 question > ')
+    console.log(question)
 
     useEffect(() => {
         question = otherQuestion;
+        question.answerValue = '이거는 변경이 가능하다고?';
         console.log(question);
-    }, [question]);
+    }, []);
+
+    const handle = () => {
+        console.log(question);
+        console.log(value);
+    }
 
     return (
         <div>
             <p>Value: {value}</p>
             <p>Question: {question.questionId}</p>
             <p>Question: {question.answerValue}</p>
+            <button onClick={handle}>버튼</button>
         </div>
     );
 };
