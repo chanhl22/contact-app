1be9d1f780acd982610743d74a6a5b59ee205feb
chanhl22 <lch9502@naver.com>
2024-11-20 18:08:06 +0900

    feat: 랜더링 순서 정리

--- src/page/useeffect-props/UseEffectProps.tsx
+++ src/page/useeffect-props/UseEffectProps.tsx
@@ -5,24 +5,33 @@ interface UseEffectPropsProps {
     question?: Question;
 }
 
-const UseEffectProps: React.FC<UseEffectPropsProps> = ({ question }) => {
+const UseEffectProps = ({ question }: UseEffectPropsProps) => {
+    // 3, 9
     const [currentValue, setCurrentValue] = useState<string>(() => '변경한 value');
+    // 3, 9
+    console.log('자식 question > ', question);
 
-    console.log('자식 question > ')
-    console.log(question)
-
+    // 5, 11
     useEffect(() => {
+        console.log('UseEffectProps > useEffect 실행')
         if (question) {
             question.answerValue = currentValue;
-            console.log('변경 question > ')
-            console.log(question);
+            console.log('변경 question > ', question);
         }
-    }, [question]);
+    // }, [question]);
+    }, []);
+
+    /**
+     * 의존성 배열에 question이 있으면 6번 순서에서 useState가 바뀌면서 재랜더링 될 때 변경을 감지해서 useEffect가 실행됨
+     *
+     * 의존성 배열에 아무것도 없으면 6번 순서에서 useState가 바뀌어도 재랜더링 될 때 변경을 감지하지 않아서 useEffect가 실행안됨
+     */
 
     const handle = () => {
         console.log(question);
     }
 
+    // 4, 10
     return question && (
         <div>
             <p>Question: {question.questionId}</p>
--- src/page/useeffect-props/UseEffectPropsPage.tsx
+++ src/page/useeffect-props/UseEffectPropsPage.tsx
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import UseEffectProps from "./UseEffectProps";
 
-
 export interface Question {
     questionId: number;
     answerValue: string;
 }
 
 const UseEffectPropsPage: React.FC = () => {
+    // 1, 7
     const [question, setQuestion] = useState<Question>();
 
+    // 6
     useEffect(() => {
-        setQuestion({questionId: 200, answerValue: "테스트"})
+        setQuestion({ questionId: 200, answerValue: "테스트" })
     }, []);
 
-    console.log('question > ')
-    console.log(question)
+    // 1, 7
+    console.log('question > ', question);
 
+    // 2, 8
     return (
         <div>
             <UseEffectProps
